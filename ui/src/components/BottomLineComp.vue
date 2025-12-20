d68baf252791ed545a876fe4b62326690d21873e
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-12-19 16:40:05 -0800

    Update BottomLineComp.vue
    Adjusted css to reflect new image size

--- ui/src/components/BottomLineComp.vue
+++ ui/src/components/BottomLineComp.vue
@@ -14,15 +14,13 @@ defineProps({
 <style scoped>
 .bottom-hr {
     width: 85%;
-    height: 59px;
+    height: 41px;
     margin-left: auto;
     margin-right: auto;
-    margin-top: -20px;
     background-image: url(../assets/scroller_right_bottom.png);
     background-repeat: no-repeat;
     background-position: center;
     background-size: 100% 100%;
     opacity: 0.6;
-    pointer-events: none;
 }
 </style>
